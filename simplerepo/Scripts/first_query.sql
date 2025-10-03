74cb4dc05621b3bc8eb3dd195e6139a5f2bb1d1a
dintaaa <dinta445@gmail.com>
2025-10-04 05:00:21 +0700

    Menghapus data para siswa

--- simplerepo/Scripts/first_query.sql
+++ simplerepo/Scripts/first_query.sql
@@ -1,9 +1,2 @@
-SELECT
-    siswa.nama,
-    AVG(nilai.nilai) AS nilai_rata_rata
-FROM
-    siswa
-JOIN
-    nilai ON siswa.id = nilai.siswa_id
-GROUP BY
-    siswa.nama;
+DELETE FROM siswa
+WHERE id IN (6, 7, 8, 9, 10);
