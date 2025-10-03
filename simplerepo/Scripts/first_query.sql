f699ffbd2ceeac2319e193ae1d377ce0045241cd
dintaaa <dinta445@gmail.com>
2025-10-04 04:43:42 +0700

    Menampilkan semua siswa

--- simplerepo/Scripts/first_query.sql
+++ simplerepo/Scripts/first_query.sql
@@ -1,9 +1,1 @@
---==[Memasukkan nilai siswa]==
-INSERT INTO nilai (siswa_id, mata_pelajaran, nilai) VALUES
-(1, 'Matematika', 85), 
-(1, 'Fisika', 88),      
-(2, 'Ekonomi', 92),     
-(2, 'Geografi', 85),    
-(3, 'Biologi', 95),     
-(4, 'Bahasa Inggris', 91), 
-(5, 'Sosiologi', 89); 
+SELECT * FROM siswa;
