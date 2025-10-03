181308a88c9dc6649eb20c82be4ba57cc67a8244
dintaaa <dinta445@gmail.com>
2025-10-04 04:15:50 +0700

    Membuat table nilai

--- simplerepo/Scripts/first_query.sql
+++ simplerepo/Scripts/first_query.sql
@@ -1,8 +1,21 @@
-CREATE TABLE siswa (
+ --==[MEMBUAT TABLE SISWA]==
+ 
+--CREATE TABLE siswa (
+--    id SERIAL PRIMARY KEY,
+--    nama VARCHAR(100) NOT NULL,
+--    umur INT,
+--    jurusan VARCHAR(50)
+--);
+
+
+
+
+--==[MEMBUAT TABLE NILAI]==
+ 
+CREATE TABLE nilai (
     id SERIAL PRIMARY KEY,
-    nama VARCHAR(100) NOT NULL,
-    umur INT,
-    jurusan VARCHAR(50)
+    siswa_id INT,
+    mata_pelajaran VARCHAR(50),
+    nilai INT,
+    FOREIGN KEY (siswa_id) REFERENCES siswa(id)
 );
-
-
