2a0746c63307c4a8792052793d221a26ffc00ffc
dintaaa <dinta445@gmail.com>
2025-10-04 04:33:48 +0700

    Memasukkan/Insert nilai ke table

--- simplerepo/Scripts/first_query.sql
+++ simplerepo/Scripts/first_query.sql
@@ -1,7 +1,9 @@
---==[Memasukkan data siswa]==
-INSERT INTO siswa (nama, umur, jurusan) VALUES
-('Dinta', 16, 'IPA'),
-('Budi', 17, 'IPS'),
-('Citra', 16, 'IPA'),
-('Dewi', 17, 'Bahasa'),
-('Eka', 16, 'IPS');
+--==[Memasukkan nilai siswa]==
+INSERT INTO nilai (siswa_id, mata_pelajaran, nilai) VALUES
+(1, 'Matematika', 85), 
+(1, 'Fisika', 88),      
+(2, 'Ekonomi', 92),     
+(2, 'Geografi', 85),    
+(3, 'Biologi', 95),     
+(4, 'Bahasa Inggris', 91), 
+(5, 'Sosiologi', 89); 
